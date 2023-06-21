790d14b373a5cc7d891d4c17b21e970eeffcb6e7
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-21 19:19:56 -0400

    Update social icons size

--- src/components/ResumeHeader.jsx
+++ src/components/ResumeHeader.jsx
@@ -3,7 +3,7 @@ const ResumeHeader = () => {
     <div>
       <h1 className="text-5xl font-bold">Hamza Abou Jaib</h1>
       <div className="w-full flex justify-end">
-        <div className="grid grid-cols-2 xl:flex xl:flex-wrap gap-y-3 xl:gap-10 w-max border-t-2 border-slate-400 pt-5 mt-5">
+        <div className="grid grid-cols-2 md:flex xl:flex-wrap gap-y-3 md:gap-5 xl:gap-10 w-max border-t-2 border-slate-400 pt-5 mt-5">
           <div className="resume-social">
             <i class="fa-solid fa-square-phone"></i>
             <div>
