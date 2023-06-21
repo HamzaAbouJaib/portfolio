de18bc392362ebc285d1e9f3e4b9537282339b3c
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-21 19:31:24 -0400

    Create a resume section layout

--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -1,19 +1,14 @@
 import ResumeHeader from "./ResumeHeader";
+import ResumeSection from "./ResumeSection";
 import ResumeSkills from "./ResumeSkills";
 
 const ResumePage = () => {
   return (
     <div className="pt-32 py-5 md:px-10 px-7 lg:w-[70%] mx-auto mb-10">
       <ResumeHeader />
-      <div className="mt-16 grid sm:grid-cols-10 gap-10">
-        <div className="w-full col-span-3">
-          <h4 className="text-3xl font-semibold">Technical Skills</h4>
-          <div className="w-[30%] border-t-2 border-blue-700 mt-4"></div>
-        </div>
-        <div className="col-span-7 w-full border-t-2 border-slate-400 pt-4">
-          <ResumeSkills />
-        </div>
-      </div>
+      <ResumeSection title={"Technical Skills"}>
+        <ResumeSkills />
+      </ResumeSection>
     </div>
   );
 };
