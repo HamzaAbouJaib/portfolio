76da0e56b216d93988543becac173c243d1a9b79
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-29 22:16:31 -0400

    Output resume as a pdf

--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -5,14 +5,24 @@ import ResumeSkills from "./ResumeSkills";
 
 const ResumePage = () => {
   return (
-    <div className="pt-32 py-5 md:px-10 px-7 lg:w-[70%] mx-auto mb-10">
-      <ResumeHeader />
+    <div className="pt-32 py-5 md:px-10 sm:px-7 lg:w-[90%] 2xl:w-[70%] mx-auto mb-10">
+      {/* <ResumeHeader />
       <ResumeSection title={"Technical Skills"}>
         <ResumeSkills />
       </ResumeSection>
       <ResumeSection title={"Experience"}>
         <ResumeExperience />
-      </ResumeSection>
+      </ResumeSection> */}
+      <object
+        data="../public/Resume.pdf"
+        type="application/pdf"
+        className="w-full h-screen"
+      >
+        <p>
+          Unable to display PDF file.{" "}
+          <a href="../public/Resume.pdf">Download</a> instead.
+        </p>
+      </object>
     </div>
   );
 };
