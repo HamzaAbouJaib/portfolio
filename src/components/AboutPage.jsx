927d677e49f00851eef8fff79881ae0e3a8b4749
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-29 23:07:45 -0400

    Create the basic layout of about section

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,7 +1,37 @@
 export const AboutPage = () => {
   return (
-    <section id="about" className="pt-32 py-5 md:px-10 px-7 mb-10">
-      AboutPage
+    <section id="about" className=" py-5 md:px-10 px-7 mb-10">
+      <h1 className="text-3xl font-semibold">About</h1>
+      <div className="flex mt-2 w-[15%] border border-blue-700" />
+      <div className="grid lg:grid-cols-2 mt-10 gap-32 max-xl:w-[80%] m-auto">
+        <div>
+          <h3 className="text-2xl font-semibold mb-5">Who am I?</h3>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            repellat dicta obcaecati a quis voluptatem quo nam ipsam fugiat
+            impedit deserunt sit quisquam maiores aliquam ut quasi quidem, et
+            dolores?
+          </p>
+          <br />
+          <p className="mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsa
+            expedita dicta quis delectus. Dolorum odit delectus corporis
+            temporibus quidem.
+          </p>
+          <a
+            href="../public/Resume.pdf"
+            target="_blank"
+            className="btn-primary"
+          >
+            View Resume
+          </a>
+        </div>
+        <div className="flex flex-col justify-between">
+          <div>
+            <h3 className="text-2xl font-semibold mb-5">Technical Skills</h3>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
