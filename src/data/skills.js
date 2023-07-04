d7c24d561f38e374f96f537f386198632e704b75
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-04 17:31:18 -0400

    Add skills

---
+++ src/data/skills.js
@@ -0,0 +1,35 @@
+const mySkills = [
+  {
+    skillType: "FrontEnd",
+    skills: [
+      "devicon-JavaScript-plain",
+      "devicon-React-plain",
+      "devicon-Angular JS-plain",
+      "devicon-TailwindCSS-plain",
+      "devicon-TypeScript-plain",
+      "devicon-HTML5-plain",
+      "devicon-CSS3-plain",
+    ],
+  },
+  {
+    skillType: "BackEnd",
+    skills: [
+      "devicon-Python-plain",
+      "devicon-Java-plain",
+      "devicon-Express-original",
+      "devicon-Node JS-plain",
+      "devicon-Next JS-plain",
+    ],
+  },
+  {
+    skillType: "Tools",
+    skills: [
+      "devicon-Git-plain",
+      "devicon-GitHub-plain",
+      "devicon-Docker-plain",
+      "devicon-Photoshop-plain",
+    ],
+  },
+];
+
+export default mySkills;
