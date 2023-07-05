a3c52e71b96d43293f52a11fc79680903215ae5f
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-05 19:28:14 -0400

    Make sections responsive

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -28,7 +28,7 @@ export const AboutPage = () => {
     <section id="about" className=" py-5 md:px-10 px-7 mb-10">
       <h1 className="text-3xl font-semibold">About</h1>
       <div className="flex mt-2 w-[15%] border border-blue-700" />
-      <div className="grid lg:grid-cols-2 mt-10 gap-32 max-xl:w-[80%] m-auto">
+      <div className="grid lg:grid-cols-2 mt-10 gap-x-32 gap-y-12 justify-between">
         <div className="flex justify-between flex-col">
           <h3 className="text-2xl font-semibold mb-5">Who am I?</h3>
           <p className="text-lg">
@@ -54,7 +54,7 @@ export const AboutPage = () => {
         <div className="flex flex-col justify-between">
           <div>
             <h3 className="text-2xl font-semibold mb-6">Technical Skills</h3>
-            <div className="grid grid-cols-3 gap-y-5">
+            <div className="grid max-sm:grid-cols-2 max-lg:grid-cols-3 max-xl:grid-cols-2 grid-cols-3 gap-y-5">
               {frontPageskills.map((skill) => (
                 <Skill name={skill} />
               ))}
@@ -64,7 +64,7 @@ export const AboutPage = () => {
             <SkillModal setShowSkillsModal={setShowSkillsModal} />
           )}
           <button
-            className="btn-secondary"
+            className="btn-secondary mt-10"
             onClick={() => setShowSkillsModal(true)}
           >
             View All Skills
