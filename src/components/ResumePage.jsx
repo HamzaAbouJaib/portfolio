745d377cd80058bf7a710c08c8f629770d7146dd
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-08 20:52:47 -0400

    Remove unused resume components

--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -1,18 +1,6 @@
-import ResumeExperience from "./ResumeExperience";
-import ResumeHeader from "./ResumeHeader";
-import ResumeSection from "./ResumeSection";
-import ResumeSkills from "./ResumeSkills";
-
 const ResumePage = () => {
   return (
     <div className="pt-32 py-5 md:px-10 sm:px-7 lg:w-[90%] 2xl:w-[70%] mx-auto mb-10">
-      {/* <ResumeHeader />
-      <ResumeSection title={"Technical Skills"}>
-        <ResumeSkills />
-      </ResumeSection>
-      <ResumeSection title={"Experience"}>
-        <ResumeExperience />
-      </ResumeSection> */}
       <object
         data="../public/Resume.pdf"
         type="application/pdf"
