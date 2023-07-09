4969f4116a2a59ddbe923770e89d404a1d8f7e1b
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-08 21:14:41 -0400

    Update introduction paragraph and font size

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,27 +1,20 @@
 const LandingPage = () => {
   return (
     <section className="flex items-center justify-center h-screen">
-      <div className="max-w-[850px] flex flex-col items-center gap-6 px-20 lg:px-10">
+      <div className="max-w-[850px] flex flex-col items-center gap-6 px-6 lg:px-10">
         <h1 className="text-4xl sm:text-5xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-500">
           Hamza Abou Jaib
         </h1>
         <h3 className="text-3xl sm:text-4xl font-bold text-center">
           Software Developer
         </h3>
-        <h5 className="text-2xl font-bold text-center text-gray-600 border-b-2 border-b-blue-500 pb-5 px-16">
+        <h5 className="text-2xl font-bold text-center text-gray-600 border-b-2 border-b-blue-500 pb-5">
           McMaster diploma
         </h5>
-        <p className="text-center text-lg max-sm:hidden">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          sapiente iste itaque dolorum illo hic ex ipsa reprehenderit rem nisi
-          labore magni, quasi accusamus ad laudantium, quos sint cumque maiores
-          fugiat obcaecati architecto, eveniet ducimus. Laborum corrupti tempore
-          placeat rem quod nobis doloribus. Debitis animi ab non, voluptatem eos
-          quidem.
-        </p>
-        <p className="text-center text-lg sm:hidden">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          voluptate nulla debitis minima incidunt possimus.
+        <p className="text-center text-xl max-w-[40rem]">
+          I am a third-year Software Engineering student at McMaster University
+          with a profound passion for programming, particularly in web
+          development.
         </p>
         <div className="flex items-center justify-center gap-4 text-[2rem]">
           <i className="fa-brands fa-linkedin hover:text-blue-500 duration-300 cursor-pointer"></i>
