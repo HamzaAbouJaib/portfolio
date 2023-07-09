60523bafdead81aee8ee4879e2f4b8b73dcf5f21
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-08 21:16:19 -0400

    Update font sizes

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,8 +1,8 @@
 export const Skill = ({ name }) => {
   return (
-    <div className="flex items-baseline gap-1">
+    <div className="flex items-baseline gap-1 text-xl">
       <i class="fa-solid text-blue-500">&#xf120;</i>
-      <p className="text-lg font-semibold text-slate-800">{name}</p>
+      <p className="text-slate-800">{name}</p>
     </div>
   );
 };
