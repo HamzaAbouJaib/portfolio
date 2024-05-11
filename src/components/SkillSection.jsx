febd6dd454a249774f3901f865ee43808b01e097
Hamza <hamza.aboujaib.0303@gmail.com>
2024-05-10 21:20:51 -0400

    Update skills

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,12 +1,18 @@
 import noImage from "../resources/noImage.png";
 
 export const SkillSection = ({ skillType, skills }) => {
+
+  const abbrMap = {
+    "AmazonWebServices": "AWS",
+    "UnifiedModelingLanguage": "UML",
+  }
+
   return (
     <div>
       <h3 className="text-2xl my-4 font-semibold">{skillType}</h3>
       <div className="flex flex-wrap gap-10 w-full max-md:place-items-center">
         {skills.map((skill) => {
-          const skillName = skill.split("-")[1] === "AmazonWebServices" ? "AWS" : skill.split("-")[1];
+          const skillName = abbrMap[skill.split("-")[1]] ? abbrMap[skill.split("-")[1]] : skill.split("-")[1];
           const skillIcon = skill.toLowerCase().replace(" ", "");
           return (
             <div className="flex flex-col justify-center items-center">
