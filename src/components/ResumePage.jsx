ae33ec0f17e0cd2dd2c434090dc486807ecf0169
Hamza <hamza.aboujaib.0303@gmail.com>
2024-05-20 21:16:12 -0400

    Fix resume link

--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -2,13 +2,13 @@ const ResumePage = () => {
   return (
     <div className="pt-32 py-5 md:px-10 sm:px-7 lg:w-[90%] 2xl:w-[70%] mx-auto mb-10">
       <object
-        data={window.location.href.includes("hamzaaboujaib.github.io") ? "/Resume.pdf" : "../public/Resume.pdf"}
+        data={window.location.href.includes("hamzaaboujaib.github.io") ? "../Resume.pdf" : "../public/Resume.pdf"}
         type="application/pdf"
         className="w-full h-screen"
       >
         <p>
           Unable to display PDF file.{" "}
-          <a className="font-bold underline" href={window.location.href.includes("hamzaaboujaib.github.io") ? "/Resume.pdf" : "../public/Resume.pdf"} target="_blank">
+          <a className="font-bold underline" href={window.location.href.includes("hamzaaboujaib.github.io") ? "../Resume.pdf" : "../public/Resume.pdf"} target="_blank">
             Download
           </a>{" "}
           instead.
